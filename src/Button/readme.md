c10e808d197fd65f49372b59deea6b0e8bec96f8
Edward Mortlock <e.mortlock@gmail.com>
2018-08-10 22:55:05 +0100

    Added base theme context & button component

--- src/Button/readme.md
+++ src/Button/readme.md
@@ -1,5 +1,54 @@
-Button example:
+Fill button:
 
 ```jsx
-<Button>Submit</Button>
+<Button fill>Primary</Button>
+<Button fill color="secondary">Secondary</Button>
+<Button fill color="danger">Danger</Button>
+<Button fill color="warning">Warning</Button>
+<Button fill color="info">Info</Button>
+```
+
+Outline button:
+
+```jsx
+<Button outline>Primary</Button>
+<Button outline color="secondary">Secondary</Button>
+<Button outline color="danger">Danger</Button>
+<Button outline color="warning">Warning</Button>
+<Button outline color="info">Info</Button>
+```
+
+Text button:
+
+```jsx
+<Button text>Primary</Button>
+<Button text color="secondary">Secondary</Button>
+<Button text color="danger">Danger</Button>
+<Button text color="warning">Warning</Button>
+<Button text color="info">Info</Button>
+```
+
+Link button:
+
+```jsx
+<Button link>Primary</Button>
+<Button link color="secondary">Secondary</Button>
+<Button link color="danger">Danger</Button>
+<Button link color="warning">Warning</Button>
+<Button link color="info">Info</Button>
+```
+
+Sizes:
+
+```jsx
+<Button large fill>Large</Button>
+<Button small fill>Small</Button>
+```
+
+Full width:
+
+```jsx
+<Button fill fullWidth>
+  Full width
+</Button>
 ```
---
+++ src/theme-context.js
@@ -0,0 +1,58 @@
+import React from 'react'
+
+export const defaultVars = {
+  radius: 'rounded',
+  spacing: {
+    sm: 2,
+    md: 4,
+    lg: 8,
+  },
+  baseColors: {
+    primaryLight: 'blue-light',
+    primary: 'blue',
+    primaryDark: 'blue-dark',
+    secondaryLight: 'grey',
+    secondary: 'grey-dark',
+    secondaryDark: 'grey-darker',
+    successLight: 'green-light',
+    success: 'green',
+    successDark: 'green-dark',
+    dangerLight: 'red-light',
+    danger: 'red',
+    dangerDark: 'red-dark',
+    warningLight: 'orange-lighter',
+    warning: 'orange-light',
+    warningDark: 'orange',
+    infoLight: 'teal-lighter',
+    info: 'teal-light',
+    infoDark: 'teal',
+  },
+  textColors: {
+    body: 'grey-darker',
+    link: 'blue-dark',
+    linkDark: 'blue-darker',
+    emphasis: 'black',
+    on: {
+      primaryLight: 'white',
+      primary: 'white',
+      primaryDark: 'white',
+      secondaryLight: 'white',
+      secondary: 'white',
+      secondaryDark: 'white',
+      successLight: 'white',
+      success: 'white',
+      successDark: 'white',
+      dangerLight: 'white',
+      danger: 'white',
+      dangerDark: 'white',
+      warningLight: 'black',
+      warning: 'black',
+      warningDark: 'black',
+      infoLight: 'black',
+      info: 'black',
+      infoDark: 'black',
+    },
+  },
+}
+
+export default React.createContext(defaultVars)
