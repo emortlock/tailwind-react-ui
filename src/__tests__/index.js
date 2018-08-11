b10871a42c004babc53ba6e7dc9d99afa1b29e80
Edward Mortlock <e.mortlock@gmail.com>
2018-08-11 18:23:43 +0100

    Ensure all components are exported

---
+++ src/__tests__/index.js
@@ -0,0 +1,15 @@
+import fs from 'fs'
+import path from 'path'
+
+describe('Library index', () => {
+  const lib = fs.readFileSync(path.join(__dirname, '..', 'index.js'), 'utf8')
+  const components = fs.readdirSync(path.join(__dirname, '..'))
+
+  components.forEach(component => {
+    if (!component.startsWith('_') && !component.includes('.')) {
+      it(`exposes ${component} for import`, () => {
+        expect(lib.match(new RegExp(component, 'g'))).not.toBe(null)
+      })
+    }
+  })
+})
