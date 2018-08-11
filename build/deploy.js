fab8ea68206a2ec0a50c2b87bad35e39b3a6a715
emortlock <edward.mortlock@gmail.com>
2018-08-11 21:10:41 +0100

    Persist node_modules

--- build/deploy.js
+++ build/deploy.js
@@ -4,4 +4,5 @@ const repository = require('../package.json').repository.url
 ghpages.publish('styleguide', {
   repo: `https://${process.env.GH_TOKEN}@${repository.replace('git+https://')}`,
   silent: true,
+  message: 'Deploy to GitHub pages [ci skip]',
 })
