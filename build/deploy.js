777d80eb980d536899d308b67515595ecd32e0cd
emortlock <edward.mortlock@gmail.com>
2018-08-12 10:55:36 +0100

    Use ghpages to set git user

--- build/deploy.js
+++ build/deploy.js
@@ -1,11 +1,14 @@
 const ghpages = require('gh-pages')
 const repository = require('../package.json').repository.url
 
+const { GH_USERNAME, GH_EMAIL, GH_TOKEN } = process.env
+
 ghpages.publish('styleguide', {
-  repo: `https://${process.env.GH_TOKEN}@${repository.replace(
-    'git+https://',
-    '',
-  )}`,
+  repo: `https://${GH_TOKEN}@${repository.replace('git+https://', '')}`,
   // silent: true,
   message: 'Deploy to GitHub pages [ci skip]',
+  user: {
+    name: GH_USERNAME,
+    email: GH_EMAIL,
+  },
 })
