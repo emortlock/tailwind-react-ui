6902f5836c0126a7ca104c08e0c30706279f9bb5
emortlock <edward.mortlock@gmail.com>
2018-08-13 07:28:21 +0100

    Added container component

---
+++ src/container/Container.md
@@ -0,0 +1,9 @@
+Makes use of the Tailwind CSS [`container plugin`](https://tailwindcss.com/docs/container).
+
+Example:
+
+```jsx
+<Container padding>
+  <div className="bg-grey p-4 h-12" />
+</Container>
+```
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 export * from './button'
 export * from './card'
+export * from './container'
 export * from './grid'
 export * from './text'
 
 export * from './config'
-
-export { default as withConfig } from './withConfig'
