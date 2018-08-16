afcc1a0adf73196a183160d6cfb92c470f0f1b2f
emortlock <edward.mortlock@gmail.com>
2018-08-16 20:50:05 +0100

    Removed debug code

--- build/styleguide.js
+++ build/styleguide.js
@@ -1,9 +1,5 @@
 import * as library from '../src'
-console.log('whaat', library)
-Object.keys(library).forEach(key => {
-  console.log(key)
 
+Object.keys(library).forEach(key => {
   global[key] = library[key]
 })
-
-console.log(global.Label)
