52bb88dc2ff5631243ff71d92eb5f51cfb03b2ba
Ed Mortlock <emortlock@users.noreply.github.com>
2018-08-19 15:25:45 +0100

    Integration tweaks (#12)
    * Corrected main file declaration
    
    * Added helper component to merge provided config with defaults
    
    * Removed max width on header container
    
    * Updated snapshot

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,4 @@
 export { default as defaultConfig } from './defaultConfig'
 export { default as TailwindConfig } from './TailwindConfig'
+export { default as TailwindConfigProvider } from './TailwindConfigProvider'
 export { default as withConfig } from './withConfig'
