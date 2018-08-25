2482387aaee04a097fef21f099492d52c6a6c8a6
emortlock <edward.mortlock@gmail.com>
2018-08-25 10:32:44 +0100

    More generic approach to getting a new colour shade

--- src/tailwind/getBaseColor.js
+++
@@ -1,7 +0,0 @@
-export default color => {
-  if (!color.includes('-')) {
-    return color
-  }
-
-  return color.substr(0, color.indexOf('-'))
-}
---
+++ src/tailwind/getColorShade.js
@@ -0,0 +1,40 @@
+const shades = [
+  'lightest',
+  'lighter',
+  'light',
+  'base',
+  'dark',
+  'darker',
+  'darkest',
+]
+
+export default (color, offset = 1) => {
+  if (offset === 0) return color
+
+  let currentColor = color === 'white' ? ['grey', 'lightest'] : color.split('-')
+  let shadeOffset = offset
+
+  if (color === 'white') {
+    if (shadeOffset < 1) return color
+    if (shadeOffset === 1) return 'grey-lightest'
+
+    currentColor = ['grey', 'lightest']
+    if (typeof shadeOffset === 'number') shadeOffset = offset - 1
+  }
+
+  if (currentColor.length === 1) {
+    currentColor.push('base')
+  }
+
+  if (typeof shadeOffset === 'string') {
+    return `${currentColor[0]}-${shadeOffset}`
+  }
+
+  const shadeIndex = Math.min(
+    Math.max(shades.indexOf(currentColor[1]) + shadeOffset, 0),
+    shades.length - 1,
+  )
+  const newShade = shades[shadeIndex]
+
+  return `${currentColor[0]}${newShade === 'base' ? '' : `-${newShade}`}`
+}
