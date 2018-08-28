6fb9373f362fa18daa6ceee5492800a158665595
emortlock <edward.mortlock@gmail.com>
2018-08-28 17:10:46 +0100

    Added visually hidden component

---
+++ src/visuallyHidden/VisuallyHidden.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import classnames from 'classnames'
+
+import { BaseComponent } from '../tailwind'
+
+const VisuallyHidden = ({ is, focusable, className, ...rest }) => (
+  <BaseComponent
+    is={is}
+    className={classnames(
+      'visually-hidden',
+      focusable && 'visually-hidden-focusable',
+      className,
+    )}
+    {...rest}
+  />
+)
+
+VisuallyHidden.propTypes = {
+  is: PropTypes.oneOfType([PropTypes.string, PropTypes.func, PropTypes.object]),
+  focusable: PropTypes.bool,
+  className: PropTypes.string,
+}
+
+VisuallyHidden.defaultProps = {
+  is: 'span',
+  focusable: false,
+  className: undefined,
+}
+
+export default VisuallyHidden
