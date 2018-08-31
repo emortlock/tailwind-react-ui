ad1a500f50b09ce27dc202b6498fc184884ff7dc
emortlock <edward.mortlock@gmail.com>
2018-08-31 15:12:08 +0100

    Added missing container className

--- src/components/container/Container.jsx
+++ src/components/container/Container.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import classnames from 'classnames'
 
 import { withTheme } from '../theme'
 import { BaseComponent } from '../tailwind'
 
-const Container = ({ theme, is, children, leftAlign, padding, ...rest }) => (
+const Container = ({
+  theme,
+  is,
+  children,
+  className,
+  leftAlign,
+  padding,
+  ...rest
+}) => (
   <BaseComponent
     is={is}
     m={!leftAlign ? { x: 'auto' } : undefined}
     p={padding ? { x: theme.spacing.md } : undefined}
+    className={classnames('container', className)}
     {...rest}
   >
     {children}
@@ -19,6 +29,7 @@ Container.propTypes = {
   theme: PropTypes.shape({}).isRequired,
   is: PropTypes.oneOfType([PropTypes.string, PropTypes.func, PropTypes.object]),
   children: PropTypes.node,
+  className: PropTypes.string,
   leftAlign: PropTypes.bool,
   padding: PropTypes.oneOfType([PropTypes.string, PropTypes.bool]),
 }
@@ -26,6 +37,7 @@ Container.propTypes = {
 Container.defaultProps = {
   is: 'div',
   children: undefined,
+  className: undefined,
   leftAlign: false,
   padding: false,
 }
