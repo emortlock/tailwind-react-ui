2b0e98ccef3447afc3e24806b68c7bfc8125a5b2
Ed Mortlock <emortlock@users.noreply.github.com>
2018-09-03 21:36:15 +0100

    Added support for `innerRef` in `BaseComponent` (#40)

--- src/components/tailwind/BaseComponent.jsx
+++ src/components/tailwind/BaseComponent.jsx
@@ -9,7 +9,14 @@ import tailwindProps, { propTypes } from './tailwindProps'
 
 const focusableElements = ['input', 'select', 'textarea', 'button', 'a']
 
-const BaseComponent = ({ is, children, className, focusable, ...rest }) => {
+const BaseComponent = ({
+  is,
+  children,
+  className,
+  focusable,
+  innerRef,
+  ...rest
+}) => {
   const Component = is
   const isFocusable = focusable || focusableElements.includes(is)
 
@@ -21,6 +28,7 @@ const BaseComponent = ({ is, children, className, focusable, ...rest }) => {
         getTailwindClassNames(rest),
         className,
       )}
+      ref={innerRef}
     >
       {children}
     </Component>
@@ -31,6 +39,7 @@ BaseComponent.propTypes = {
   is: PropTypes.oneOfType([PropTypes.string, PropTypes.func, PropTypes.object]),
   children: PropTypes.node,
   className: PropTypes.string,
+  innerRef: PropTypes.oneOfType([PropTypes.func, PropTypes.object]),
   ...propTypes,
 }
 
@@ -38,6 +47,7 @@ BaseComponent.defaultProps = {
   is: 'div',
   children: undefined,
   className: undefined,
+  innerRef: undefined,
 }
 
 export default BaseComponent
