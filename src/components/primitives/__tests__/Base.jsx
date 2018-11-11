2e2b48c09be823c2f532e4c423e1552a22b57738
emortlock <edward.mortlock@gmail.com>
2018-11-11 21:43:22 +0000

    Updated tests

--- src/components/primitives/__tests__/Base.jsx
+++ src/components/primitives/__tests__/Base.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import { shallow } from 'enzyme'
 
 import { component as Base } from '../Base'
+import { defaultTheme } from '../../theme'
 
 const setup = (testProps = {}) => {
-  const props = Object.assign({}, testProps)
+  const props = Object.assign({ theme: defaultTheme }, testProps)
 
   const wrapper = shallow(<Base {...props}>Hello World</Base>)
 
